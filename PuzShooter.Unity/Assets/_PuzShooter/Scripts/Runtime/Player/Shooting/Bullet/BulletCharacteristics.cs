08c714120b967ad83eec71de37c35095e75c039c
Damir <139867315+mondlicht0@users.noreply.github.com>
2024-01-06 00:22:09 +0600

    feature enemy shooting

--- PuzShooter.Unity/Assets/_PuzShooter/Scripts/Runtime/Player/Shooting/Bullet/BulletCharacteristics.cs
+++ PuzShooter.Unity/Assets/_PuzShooter/Scripts/Runtime/Player/Shooting/Bullet/BulletCharacteristics.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Bullet Type", menuName = "Bullet/Bullet Type")]
 public class BulletCharacteristics : ScriptableObject
 {
+    public bool IsPlayersBullet;
     public float Speed = 10f;
 
     public bool IsBouncy;
