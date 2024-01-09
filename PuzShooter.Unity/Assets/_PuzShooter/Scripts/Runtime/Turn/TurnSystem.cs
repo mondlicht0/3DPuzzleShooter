687de82df5230e670dfe807b34277c111bbb1f6e
Damir <139867315+mondlicht0@users.noreply.github.com>
2024-01-09 22:11:09 +0600

    feature puzzle element

---
+++ PuzShooter.Unity/Assets/_PuzShooter/Scripts/Runtime/Turn/TurnSystem.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading;
+using Cysharp.Threading.Tasks;
+
+public class TurnSystem : MonoBehaviour
+{
+    private Shooting _playerShooting;
+    private List<Enemy> _enemyList = new List<Enemy>();
+
+    [SerializeField, Range(1f, 8f)]
+    private float _playerTurnTime;
+    private bool _isPlayerShot = false;
+    private bool _isPlayerTurn = true;
+    private bool _isEnemyTurn = false;
+
+    public bool IsPlayerTurn { get { return _isPlayerTurn; } }
+    public bool IsEnemyTurn { get { return _isEnemyTurn; } }
+
+    private void Start()
+    {
+        PlayerFacade.Shooting.OnShoot += EnemyTurn;
+
+        InitEnemies();
+    }
+
+    public void PlayerTurn()
+    {
+        Debug.Log("Player Turn");
+    }
+
+    private void EnemyTurn()
+    {
+        Debug.Log("ENemy Turn");
+
+        _isEnemyTurn = true;
+
+        InitEnemies();
+
+        foreach (Enemy enemy in _enemyList)
+        {
+            enemy.ShootToPlayer();
+        }
+
+        CheckEnemyShots(_enemyList);
+    }
+
+    private void CheckEnemyShots(List<Enemy> _enemyList)
+    {
+        foreach (Enemy enemy in _enemyList)
+        {
+            if (!enemy.IsShot) return;
+        }
+
+        _isEnemyTurn = false;
+        PlayerTurn();
+    }
+
+    private void InitEnemies()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        _enemyList.Clear();
+        _enemyList.AddRange(enemies);
+    }
+}
